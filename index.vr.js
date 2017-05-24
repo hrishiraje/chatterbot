990e334d232560fa46273291c8f2e6a9d818f18f
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-24 09:19:38 -0700

    added vr init commit

---
+++ index.vr.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { AppRegistry, asset, Pano, View, Text, StyleSheet, Box } from 'react-vr';
+
+class Basics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontSize: 0.1
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Pano source={asset('test3.jpg')}></Pano>
+        <Text onEnter={() => this.setState({ fontSize: 0.2 })} onExit={() => this.setState({ fontSize: 0.1 })} style={{ fontSize: this.state.fontSize, transform: [{ translate: [0, 0, -2] }] }}>Hover Over Me Plox</Text>
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('VRBasics', () => Basics);
+
