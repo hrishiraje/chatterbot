6a45d5edda7c424d9ca51ca8d1aacedaca5865b6
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-24 14:34:44 -0700

    connected express to react ver

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var parser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var path = require('path');
-var router = require('./router/router.js');
+var router = require('./routes/router.js');
 
 var app = express();
 app.set('port', process.env.PORT || 9000);
@@ -11,7 +11,7 @@ app.use(parser.json());
 app.use(parser.urlencoded({extended: true}));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../vr/')));
 app.use(router);
 
 if (!module.parent) {
