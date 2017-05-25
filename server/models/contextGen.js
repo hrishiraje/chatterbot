ab9d91bcd8fe5da4f10a2e69027c9284b0967823
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-25 14:40:30 -0700

    added skeleton code for context

--- server/models/contextGen.js
+++ server/models/contextGen.js
@@ -3,7 +3,32 @@ const contextData = require('./context.js');
 // context is the message context
 
 module.exports = function(message, context, cb) {
-  cb('initial order', 'greeting', 'question'); 
+  var contexts = [];
+  for (let i = 0; i < message.length; i++) {
+    for (let key in contextData) {
+      for (let k = 0; k < contextData[key][k]; k++) {
+        if (message[i] === contextData[key][k]) {
+          if (contexts.indexOf(contextData[key]) === -1) {
+            contexts.push(contextData[key]);
+          }
+        }
+      }
+    }
+  }
+
+  if (contexts.length === 0) {
+    cb(null, context, 'statement');
+  } else if (contexts.length === 1) {
+    var calculatedPunctuation = punct(contexts[0]);
+    cb(contexts[0], context, calculatedPunctuation);
+  } else if (contexts.length > 1) {
+
+  }
+
 };
 
+
+var punct = function(statement) {
+  
+};
 
