f56118dbbaffae16cacc8842ff6d97090286524e
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-25 19:35:06 -0700

    added post route, controller and model skeleton

--- server/routes/router.js
+++ server/routes/router.js
@@ -5,4 +5,6 @@ router.get('/api/response', controllers.robot.responseGet);
 
 router.get('/api/startup', controllers.robot.startup);
 
+router.post('api/placeOrder', controllers.robot.placeOrder);
+
 module.exports = router;
