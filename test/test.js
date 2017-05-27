e89d41dd4c245352bc87393148da20aea35d4ac3
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-26 19:01:56 -0700

    added context gen tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ var chaiHttp = require('chai-http');
 var chai = require('chai');
 var should = chai.should();
 var punctuation = require('../server/models/contextGen').punctuation;
+var contextGen = require('../server/models/contextGen').contextGen;
 
 chai.use(chaiHttp);
 
@@ -16,7 +17,7 @@ describe('server', function() {
       res.should.have.status(200);
       done();
     });
-    });
+  });
 
   it('should send an object back containing the response from the server after calling a GET request to "/api/startup"', function(done) {
     chai.request('http://127.0.0.1:9000')
@@ -32,7 +33,7 @@ describe('server', function() {
         done();
       });
   });
-   it('should respond to GET requests for "/api/response" + url client response with 200 status code and receieve an object with the robots responses', function(done) {
+  it('should respond to GET requests for "/api/response" + url client response with 200 status code and receieve an object with the robots responses', function(done) {
     chai.request('http://127.0.0.1:9000')
       .get('/api/response?message=yes&context=newOrder')
       .end((err, res) => {
@@ -58,20 +59,48 @@ describe('server', function() {
 
 });
 
-describe('punctuation', function(){
-  it('should return question for ?', function(){
+describe('punctuation', function() {
+  it('should return question for ?', function() {
     var result = punctuation(['large', 'pizza', '?']);
     expect(result).to.equal('question');
   });
 
-  it('should return question for a question word', function(){
+  it('should return question for a question word', function() {
     var result = punctuation(['why', 'pizza', '?']);
     expect(result).to.equal('question');
   });
 
-  it('should return statement if no question terms included', function(){
+  it('should return statement if no question terms included', function() {
     var result = punctuation(['large', 'pizza']);
     expect(result).to.equal('statement');
   });
 });
 
+
+describe('calculate context', function () {
+  it('should return no contexts when none of the words match', function () {
+    contextGen(['my', 'name'], 'toppings', function (calculatedContext, context, punctuation) {
+      expect(calculatedContext).to.equal(null);
+    });
+  });
+
+  it('should return a single context when one word matches', function () {
+    contextGen(['order', 'name'], 'toppings', function (calculatedContext, context, punctuation) {
+      expect(calculatedContext).to.equal('newOrder');
+    });
+  });
+
+  it('should return the correct expected context when there are multiple potential contexts', function () {
+    contextGen(['order', 'pepperoni'], 'newOrder', function (calculatedContext, context, punctuation) {
+      expect(calculatedContext).to.equal('newOrder');
+    });
+  });
+
+  it('should handle assertion correctly', function () {
+    contextGen(['absolutely'], 'newOrder', function (calculatedContext, context, punctuation) {
+      expect(calculatedContext).to.equal('newOrder');
+    });
+  });
+
+
+});
