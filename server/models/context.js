2b3a53f0557095bd6636c129e8098f3540276457
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-27 17:06:37 -0700

    began changes to server side

--- server/models/context.js
+++ server/models/context.js
@@ -1,11 +1,15 @@
 module.exports = {
   greeting: ['hi', 'hello', 'what’s', 'up', 'been', 'you', 'what', 'sup', 'yo', 'good'],
-  newOrder: ['new', 'order', 'pizza', 'want', 'to', 'get', 'can', 'i'],
-  pizzaType: ['small', 'medium', 'large', 'extra large', 'L', 'l', 'xl', 'x-l', 'm', 's'],
-  topping: ['pepperoni', 'mushrooms', 'mushroom', 'linguica', 'anchovies', 'olives', 'olive', 'veggies', 'veggie', 'American Cheese', 'Banana Peppers', 'Black Olives', 'Cheddar Cheese', 'Cheese', 'Diced Tomatoes', 'Feta Cheese', 'Green Peppers', 'Hot Sauce', 'Jalapeno Peppers', 'Mushrooms', 'Onions', 'Pineapple', 'Roasted Red Peppers', 'Shredded Parmesan', 'Shredded Provolone Cheese', 'Sliced Provolone', 'Spinach'],
-  sauce: ['Alfredo Sauce', 'BBQ Sauce', 'Balsamic', 'Blue Cheese', 'Caesar', 'Icing', 'Kicker Hot Sauce', 'Marinara', 'Ranch', 'Sweet Mango Habanero Sauce'],
+  newOrder: ['new', 'order', 'pizza', 'want'],
+  hungerLevel: ['very', 'little', 'lot', 'decent', 'medium', 'starving', 'ravenous', 'super', 'hella', 'eh', 'moderately', 'moderate'],
+  totalPeople: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20'],
+  confirmCount: [],
+  toppings: [],
+  sides: [],
+  drinks: [],
   finalOrder: [],
   assertion: ['yes', 'yeah', 'yah', 'ya', 'sure', 'absolutely', 'definitely'],
   negation: ['no', 'nah', 'na', 'nope', 'not'],
-  restart: ['start']
+  restart: ['start'],
+  quit: ['exit', 'cancel', 'stop', 'quit', 'terminate']
 };
--- server/models/contextGen.js
+++ server/models/contextGen.js
@@ -20,10 +20,8 @@ module.exports.punctuation = function(wordArray) {
   return found ? 'question' : 'statement';
 };
 
+
 module.exports.contextGen = function(message, context, cb) {
-
-  // module.exports.punctuation(['hello']);
-
   var contexts = [];
   for (let i = 0; i < message.length; i++) {
     for (let key in contextData) {
@@ -37,59 +35,100 @@ module.exports.contextGen = function(message, context, cb) {
     }
   }
 
-  console.log(contexts);  
+  var punct = module.exports.punctuation(message);
 
   if (contexts.length === 0) {
-
-    cb(null, context, 'statement');
-
+    cb(null, context, punct);
   } else if (contexts.length === 1) {
-
-    if (contexts[0] === 'assertion') {
-      contexts[0] = context;
-    }
-    
-    // else if (contexts[0] === 'negation') {
-    //   contexts[0] = 'restart';
-    // }
-
-    var calculatedPunctuation = module.exports.punctuation(message);
-    console.log('calculatedPunction ', calculatedPunctuation);
-    cb(contexts[0], context, calculatedPunctuation);
-
-  } else if (contexts.length > 1) {
-    // var sum = 0;
-    // var calculation = 0;
-
-    // contexts.forEach((contextname) => {
-    //   sum += contextValue[contextname];
-    // });
-
-    // calculation = Math.round(sum / contexts.length);
-    // var returnContext;
-
-    // for (let key in contextValue) {
-    //   if (contextValue[key] === calculation) {
-    //     returnContext = key;
-    //   }
-    // }
-
-
-    // if (calculatedContext === 'assertion') {
-    //   calculatedContext = returnContext;
-    // }
+    cb(contexts[0], context, punct);
+  } else {
     var calculatedContext = '';
     if (contexts.includes(context)) {
       calculatedContext = context;
     } else {
       calculatedContext = null;
     }
-    console.log('What we are sending in fam', calculatedContext, context);
-
-
-    cb(calculatedContext, context, 'statement');
+    cb(calculatedContext, context, punct);
   }
+
 };
+
+
+
+
+
+
+
+// module.exports.contextGen = function(message, context, cb) {
+
+//   // module.exports.punctuation(['hello']);
+
+//   var contexts = [];
+//   for (let i = 0; i < message.length; i++) {
+//     for (let key in contextData) {
+//       for (let k = 0; k < contextData[key].length; k++) {
+//         if (message[i] === contextData[key][k]) {
+//           if (contexts.indexOf(contextData[key]) === -1) {
+//             contexts.push(key);
+//           }
+//         }
+//       }
+//     }
+//   }
+
+//   console.log(contexts);  
+
+//   if (contexts.length === 0) {
+
+//     cb(null, context, 'statement');
+
+//   } else if (contexts.length === 1) {
+
+//     if (contexts[0] === 'assertion') {
+//       contexts[0] = context;
+//     }
+    
+//     // else if (contexts[0] === 'negation') {
+//     //   contexts[0] = 'restart';
+//     // }
+
+//     var calculatedPunctuation = module.exports.punctuation(message);
+//     console.log('calculatedPunction ', calculatedPunctuation);
+//     cb(contexts[0], context, calculatedPunctuation);
+
+//   } else if (contexts.length > 1) {
+//     // var sum = 0;
+//     // var calculation = 0;
+
+//     // contexts.forEach((contextname) => {
+//     //   sum += contextValue[contextname];
+//     // });
+
+//     // calculation = Math.round(sum / contexts.length);
+//     // var returnContext;
+
+//     // for (let key in contextValue) {
+//     //   if (contextValue[key] === calculation) {
+//     //     returnContext = key;
+//     //   }
+//     // }
+
+
+//     // if (calculatedContext === 'assertion') {
+//     //   calculatedContext = returnContext;
+//     // }
+//     var calculatedContext = '';
+//     if (contexts.includes(context)) {
+//       calculatedContext = context;
+//     } else {
+//       calculatedContext = null;
+//     }
+//     console.log('What we are sending in fam', calculatedContext, context);
+
+
+//     cb(calculatedContext, context, 'statement');
+//   }
+// };
 
 
 
