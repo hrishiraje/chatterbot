ac4654ae1b30127b6eb57d7f4299b4cfc134e671
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-30 15:53:54 -0700

    added floating toppings test

---
+++ components/topping.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Topping extends Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+}
+
+export default Topping;
