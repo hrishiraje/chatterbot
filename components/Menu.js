a3a7454c2930a0561ce937aa3ea35714af6791b0
ArseniyKotov <kotov.a.arseniy@gmail.com>
2017-05-30 17:57:52 -0700

    added floating toppings

--- components/Menu.js
+++ components/Menu.js
@@ -2,95 +2,98 @@ import React from 'react';
 import {Animated, asset, Image, View, VrButton, Model, StyleSheet} from 'react-vr';
 import axios from 'axios';
 
-const styles = StyleSheet.create({
+
+
+var styles = StyleSheet.create({
   toppingImage: {
-    flex: 1,
     width: 1,
     height: 1,
     borderRadius: 0.25,
-    borderWidth: 0.02
+    borderWidth: 0.05,
+    margin: 0.2
   },
   container1: {
-    flex: 1, 
+    flex: 1,
     flexDirection: 'row', 
-    width: 2, 
-    alignItems: 'stretch', 
+    width: 1, 
+    height: 1,
+    alignItems: 'stretch',
     transform: [
       { 
-        translate: [-0.5, 2, 0.866] 
+        translate: [-3, 3, -3]
       }, 
       { 
-        rotateY: 0 
+        rotateX: 45
       }
     ]
   },
   container2: {
-    flex: 1,
+    
     flexDirection: 'row',
     width: 2,
     alignItems: 'stretch',
     transform: [
       {
-        translate: [-7, 2, -1]
+        translate: [2, 4, -3]
       },
       {
-        rotateY: 90
+        rotateX: 0
       }
     ]
   },
   container3: {
-    flex: 1,
+    
     flexDirection: 'row',
     width: 2,
     alignItems: 'stretch',
     transform: [
       {
-        translate: [-7, 1, -1]
+        translate: [0, 4, -3]
       },
       {
-        rotateY: 90
+        rotateX: 0
       }
     ]
   },
   container4: {
-    flex: 1,
+    
     flexDirection: 'row',
     width: 2,
     alignItems: 'stretch',
     transform: [
       {
-        translate: [-7, 1, -1]
+        translate: [-2, 4, -3]
       },
       {
-        rotateY: 90
+        rotateX: 0
       }
     ]
   },
   container5: {
-    flex: 1,
+    
     flexDirection: 'row',
     width: 2,
     alignItems: 'stretch',
     transform: [
       {
-        translate: [-7, 1, -1]
+        translate: [-4, 4, -3]
       },
       {
-        rotateY: 90
+        rotateX: 0
       }
     ]
   },
   container6: {
-    flex: 1,
+    
     flexDirection: 'row',
     width: 2,
     alignItems: 'stretch',
     transform: [
       {
-        translate: [-7, 1, -1]
+        translate: [-6, 4, -3]
       },
       {
-        rotateY: 90
+        rotateX: 0
       }
     ]
   }
@@ -134,6 +137,7 @@ class Menu extends React.Component {
     this.submit = this.submit.bind(this);
   }
   componentDidMount() {
+    
     this.state.bounceValue.setValue(1.5);     // Start large
     Animated.spring(                          // Base: spring, decay, timing
       this.state.bounceValue,                 // Animate `bounceValue`
@@ -160,14 +164,16 @@ class Menu extends React.Component {
 
   render() {
     return (
-      <View>
-        <View style={styles.container1}>
+      <View style={styles.container1}>
+        <View>
           <VrButton onClick={()=>this.setState({pepperoni:{selected: !this.state.pepperoni.selected, code: this.state.pepperoni.code}})}>
             <Animated.Image                         // Base: Image, Text, View
               source={{uri: 'http://i.imgur.com/ISykk1n.jpg'}}
               style={[ styles.toppingImage,  {transform: [ {scale: this.state.bounceValue} ]}]}
             />
           </VrButton>
+        </View>
+        <View>
           <VrButton onClick={()=>this.setState({italianSausage:{selected: !this.state.italianSauage.selected, code: this.state.italianSausage.code}})}>
             <Animated.Image                         // Base: Image, Text, View
               source={{uri: 'http://i.imgur.com/8fBPAY7.jpg'}}
@@ -175,13 +181,15 @@ class Menu extends React.Component {
             />
           </VrButton>
         </View>
-        <View style={styles.container2}>
+        <View>
           <VrButton onClick={()=>this.setState({beef:{selected: !this.state.beef.selected, code: this.state.beef.code}})}>
             <Animated.Image                         // Base: Image, Text, View
               source={{uri: 'http://3.bp.blogspot.com/-IY5KYLYaTHY/UukyyqLZv8I/AAAAAAAGe9I/MHayfAqSI9g/s1600/DOMINO%27S+PIZZA+Medium+Beef+12+Inch+1+Lb.+7+Oz.+Pie,+Domino%27s+Pizza+Delivery+Take+Out+Store+Restaurant+Medium+Beef+Pizza.JPG'}}
               style={[styles.toppingImage, { transform: [{ scale: this.state.bounceValue }] }]}
             />
           </VrButton>
+        </View>
+        <View>
           <VrButton onClick={()=>this.setState({ham:{selected: !this.state.ham.selected, code: this.state.ham.code}})}>
             <Animated.Image                         // Base: Image, Text, View
               source={{uri: 'https://knightstemplarinternational.com/wp-content/uploads/2016/08/Ham-Pizza-2.jpg'}}
@@ -189,14 +197,16 @@ class Menu extends React.Component {
             />
           </VrButton>
         </View>
-        <View style={styles.container3}>
+        <View>
           <VrButton onClick={()=>this.setState({bacon:{selected: !this.state.bacon.selected, code: this.state.bacon.code}})}>
             <Animated.Image                         // Base: Image, Text, View
               source={{uri: 'https://knightstemplarinternational.com/wp-content/uploads/2017/03/Bacon.jpg'}}
               style={[styles.toppingImage, { transform: [{ scale: this.state.bounceValue }] }]}
             />
           </VrButton>
-            <VrButton onClick={()=>this.submit()}>
+        </View>
+        <View>
+          <VrButton onClick={()=>this.submit()}>
             <Animated.Image                         // Base: Image, Text, View
               source={{uri: 'http://i.imgur.com/t1dfqfF.png'}}
               style={[styles.toppingImage, { transform: [{ scale: this.state.bounceValue }] }]}
--- components/topping.js
+++ components/topping.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { Animated, asset, Image, View, VrButton, Model, StyleSheet } from 'react-vr';
 
 class Topping extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selected: false
+    }
+  }
+
+  addSelection() {
+    this.setState({
+      selected: !this.state.selected
+    });
+
+    
   }
   
   render() {
     return (
-      <View>
-        
+      <View style={{ borderColor: this.satte.selected === true ? 'red' : 'black', transform: [{ translate: this.props.location }]}}>
+        <Image onClick={this.addSelection().bind(this)} />
       </View>
     );
   }
