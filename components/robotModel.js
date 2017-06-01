9ac5ddbec982479cbc18a57f51848d7b3dcf83ea
Simmba <simmern.kang@gmail.com>
2017-06-01 12:27:59 -0700

    not working robo animations

--- components/robotModel.js
+++ components/robotModel.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import { View, Model, asset } from 'react-vr'
+import { View, Model, asset, Animated } from 'react-vr'
 import BouncingText from './bouncingText';
 
- 
+const AnimatedModel = Animated.createAnimatedComponent(Model);
 class RobotModel extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      bounceValue: new Animated.Value(0.5),
+       hoverValue: new Animated.Value(-0.3),
+       rotate: function (){
+   Animated.sequence([
+    Animated.timing(this._animatedValue, {
+        toValue: -.3,
+        duration: 1000
+    }),
+    Animated.timing(this._animatedValue,{
+      toValue: -.7,
+      duration: 1000
+    })
+    ]).start((event)=> {if(event.finished){this.state.rotate()}});
+   },
+
+    };
   }
 
+componentWillMount() {
+    this._animatedValue = new Animated.Value(-.7);
+  }
+  componentDidMount() {
+   this.state.rotate()
+  }
   render() {
+     var rotate = this._animatedValue.interpolate({
+        inputRange: [0, 100],
+      outputRange: ['0deg', '360deg']
+    });
+
     return (
       <View>        
-        <BouncingText theText={this.props.robotText} />
-        <Model style={{ transform: [{ translate: [1, -0.3, -3] }, { scale: 0.7 }] }} source={{ obj: asset('br1.obj'), mtl: asset('br1.mtl')}}/>
+        <BouncingText theText={this.props.robotText} rotate={this.state.rotate} />
+        <AnimatedModel style={{ transform: [{ translate: [1, -.3, -3] }, { scale: 0.7 }, {rotate: rotate}, {translateY: this._animatedValue}] }} source={{ obj: asset('br1.obj'), mtl: asset('br1.mtl')}}/>
       </View>
     );
   }
